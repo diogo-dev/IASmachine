fd0f9ef08cc903fbe78adda518c219fceb258a0f
Diogo Felipe <diogofelipe.dev@gmail.com>
2023-12-01 19:03:01 -0300

    Primeiro commit

---
+++ IASmachine.c
@@ -0,0 +1,10 @@
+/*      Trabalho de Arquitetura 1
+Desenvolver um simulador do computador IAS
+
+Alunos: Diogo Felipe Soares da Silva    RA:124771
+        Arthur Henrique Bando Ueda      RA:129406
+        Gustavo Alves Glatz             RA:128592
+*/
+#include <stdio.h>
+#include <string.h>
+
