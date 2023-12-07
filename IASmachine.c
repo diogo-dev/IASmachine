cb831bca19bc77cf2ab89addd6f8c7214bcd10aa
Diogo Felipe <diogofelipe.dev@gmail.com>
2023-12-06 22:53:40 -0300

    Primeiras manipulações do trabalho

--- IASmachine.c
+++ IASmachine.c
@@ -6,5 +6,45 @@ Alunos: Diogo Felipe Soares da Silva    RA:124771
         Gustavo Alves Glatz             RA:128592
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+//para simular a memoria do IAS devemos alocar um bloco de memoria, como se fosse um vetor
+//char *memory = (char *) malloc(4096 * 5 * sizeof(char));
+
+void read_line(char str[], int size, FILE *arq)
+{
+        int i = 0;
+        char c = fgetc(arq);
+        while(c != '\n')
+        {
+               if (i < size-1)
+               {
+                       str[i] = c;
+                       i++;
+               }
+               c = fgetc(arq);
+        }
+        str[i] = '\0';
+}
+
+int main()
+{
+        char c;
+        FILE *arq;
+
+        if ((arq = fopen("texto.txt", "r")) == NULL)
+        {
+                printf("Erro ao abrir o aquivo!");
+                exit(1);
+        }
+
+        while((c = fgetc(arq)) != EOF)
+        {
+                fputc(c, stdout);
+        }
+
+        fclose(arq);
+
+        return 0;
+}
---
+++ teste.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *x = (int *) malloc(10 * sizeof(int));
+    //char *m = (char *) malloc(4096 * 5 * sizeof(char));
+
+    if(x)
+    {
+        printf("Memoria alocada com sucesso\n");
+
+        printf("\nx: %d", x);
+        printf("\nx: %d", *x);
+        *x = 50;
+        printf("\nx: %d", *x);
+        x += 1;
+        printf("\nx: %d", x);
+        printf("\nx: %d", *x);
+        *x = 100;
+        printf("\nx: %d", *x);
+        x -= 1;
+        printf("\nx: %d", x);
+        printf("\nx: %d", *x);
+    }
+    else{
+        printf("Nao foi possivel alocar a memoria");
+    }
+
+    free(x);
+
+    return 0;
+}
