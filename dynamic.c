c5df93c15885760bfa44bdcb4b9e2aae6100fd8c
Gustavo Glatz <gustavoglatz123@gmail.com>
2023-12-14 23:56:10 -0300

    Finding_addres

--- dynamic.c
+++ dynamic.c
@@ -12,56 +12,36 @@ Alunos: Diogo Felipe Soares da Silva    RA:124771
 #define BUFFER_SIZE 40
 // para simular a memoria do IAS devemos alocar um bloco de memoria, como se fosse um vetor
 // char *memory = (char *) malloc(4096 * 5 * sizeof(char));
-
-char finding_addres(char *str_lida, char* no_addres){
-    for(int i=0; i=strlen(str_lida); i++){
-        if(str_lida[i] == '('){
-            return str_lida[i+1];
+    
+void finding_addres(char *str_lida, char *addres){
+    const char delimitador1[2] = ")";
+    const char delimitador2[2] = "(";
+    char *token1;
+    char *token2;
+    
+    int aux;
+    for(int i=0; i<=strlen(str_lida); i++){
+        if(str_lida[i] != '('){
+            aux++;
         }
-    } 
-    *no_addres = "000000000000";
-    return 0;
+    }
+
+    if(aux == strlen(str_lida)){
+        token1 = strtok(str_lida, delimitador1);
+        while(token1 != NULL){
+            token2 = strtok(token1, delimitador2);
+            while(token2 != NULL){
+                strcpy(addres, token2);
+                token2 = strtok(NULL, delimitador2);
+                token1 = strtok(NULL, delimitador1);
+            }
+        }
+    }
+    else{
+        strcpy(addres, "000000000000");
+    }
 }
 
-
-long int decoder(long int number)
-{
-    int number_size = 0;
-    long int temp_number = number, to_return = 0, power_2 = 1;
-
-    // ------------ Transformando um inteiro em um vetor de inteiro --------------
-
-    while (temp_number != 0)
-    {
-
-        temp_number /= 10;
-        number_size++;
-    }
-
-    long int numberArray[number_size];
-    int temp = (number_size - 1);
-    temp_number = number;
-
-    while (temp_number != 0)
-    {
-
-        numberArray[temp] = temp_number % 10;
-        temp_number /= 10;
-        temp--;
-    }
-
-    // ------------ Transformando um inteiro em um vetor de inteiro --------------
-
-    // Realizar transformacao do vetor binario em um inteiro decimal
-
-    for (int i = number_size; i >= 0; i--)
-    {
-        to_return += numberArray[i] * power_2;
-        power_2 *= 2;
-    }
-
-    return to_return;
-}
 
 void display_memory_data(char **memory)
 {
@@ -101,7 +81,7 @@ void finding_instruction(char *str_lida, char *opcode)
     }
     else if (strcmp(str_lida, "LOAD-|M(X)|") == 0) {
         strcpy(opcode, "00000011");
-    }
+    }    
     else if (strcmp(str_lida, "LOAD--|M(X)|") == 0) {
         strcpy(opcode, "00000100");
     }
@@ -166,25 +146,27 @@ int main()
         exit(1);
     }
 
-    int i = 0;
+    char address[12];
+    int i=0;
     while (fgets(linha_lida, BUFFER_SIZE, arq) != NULL)
     {
-        linha_lida[strlen(linha_lida) - 1] = '\0'; // retirando o '\n' da string lida
-        memory[i] = (char *)malloc(5 * sizeof(char));
-        strcpy(memory[i], linha_lida);
-        i++; // equivalente a memory += 5;
+        if (i < 500)
+        {
+            linha_lida[strlen(linha_lida) - 1] = '\0'; // retirando o '\n' da string lida
+            memory[i] = (char *)malloc(5 * sizeof(char));
+            strcpy(memory[i], linha_lida);
+            i++;
+        }
+        else
+        {
+            linha_lida[strlen(linha_lida) - 1] = '\0'; // retirando o '\n' da string lida
+            finding_addres(linha_lida, address);
+            printf("%s\n", address);
+        }
     }
 
-    display_memory_data(memory);
-    display_memory_instructions(memory);
-    printf("%s\n", opcode);
-    finding_instruction(memory[500], opcode);
-    printf("%s\n", opcode);
-    finding_instruction(memory[501], opcode);
-    printf("%s\n", opcode);
-
-    //char *trash;
-    //printf("%d", (strtol(opcode, &trash, 2)));
+    //display_memory_data(memory);
+    //display_memory_instructions(memory);
 
     free(memory);
     fclose(arq);
